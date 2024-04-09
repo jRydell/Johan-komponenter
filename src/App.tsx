7b66bce3f8d42f9216634624c021b6856af07ff9
93mcqmil <mcqueen_1993@hotmail.com>
2024-04-09 14:33:28 +0200

    button component with Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.scss";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
 import Todo from "./components/Todo/Todo";
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
     <>
       <h1>Komponenter</h1>
       <Todo />
+      <Header />
+      <Form />
     </>
   );
 }
